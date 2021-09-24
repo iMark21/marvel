37cdeb160717fb7dd24a27d7be85e9487939f057
Michel Marques <marques.jm@icloud.com>
2021-09-24 12:47:37 +0200

    [develop] Swift file not updated in git

---
+++ marvel/marvel/App/Extensions/RegisterCells+Extension.swift
@@ -0,0 +1,62 @@
+//
+//  UICollectionView+Extension.swift
+//  marvel
+//
+//  Created by Michel Marques on 24/9/21.
+//
+
+import Foundation
+import UIKit
+
+extension UICollectionView {
+    /**
+     Register nibs faster by passing the type - if for some reason the `identifier` is different then it can be passed
+     - Parameter type: UICollectionView.Type
+     - Parameter identifier: String?
+     */
+    func registerCell(type: UICollectionViewCell.Type, identifier: String? = nil) {
+        let cellId = String(describing: type)
+        register(UINib(nibName: cellId, bundle: nil), forCellWithReuseIdentifier: identifier ?? cellId)
+    }
+    
+    /**
+     DequeueCell by passing the type of UICollectionViewCell and IndexPath
+     - Parameter type: UICollectionViewCell.Type
+     - Parameter indexPath: IndexPath
+     */
+    func dequeueCell<T: UICollectionViewCell>(withType type: UICollectionViewCell.Type, for indexPath: IndexPath) -> T? {
+        return dequeueReusableCell(withReuseIdentifier: type.identifier, for: indexPath) as? T
+    }
+}
+
+public extension UITableView {
+    
+    /**
+     Register nibs faster by passing the type - if for some reason the `identifier` is different then it can be passed
+     - Parameter type: UITableViewCell.Type
+     - Parameter identifier: String?
+     */
+    func registerCell(type: UITableViewCell.Type, identifier: String? = nil) {
+        let cellId = String(describing: type)
+        register(UINib(nibName: cellId, bundle: nil), forCellReuseIdentifier: identifier ?? cellId)
+    }
+    
+    /**
+     DequeueCell by passing the type of UITableViewCell
+     - Parameter type: UITableViewCell.Type
+     */
+    func dequeueCell<T: UITableViewCell>(withType type: UITableViewCell.Type) -> T? {
+        return dequeueReusableCell(withIdentifier: type.identifier) as? T
+    }
+    
+    /**
+     DequeueCell by passing the type of UITableViewCell and IndexPath
+     - Parameter type: UITableViewCell.Type
+     - Parameter indexPath: IndexPath
+     */
+    func dequeueCell<T: UITableViewCell>(withType type: UITableViewCell.Type, for indexPath: IndexPath) -> T? {
+        return dequeueReusableCell(withIdentifier: type.identifier, for: indexPath) as? T
+    }
+    
+}
+
